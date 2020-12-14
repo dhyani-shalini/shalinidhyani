51459110ec51569ce7c49986b674289816ad2bf4
dhyani-shalini <76002127+dhyani-shalini@users.noreply.github.com>
2020-12-14 22:58:16 +0530

    Add files via upload

---
+++ image/script.js
@@ -0,0 +1,139 @@
+/*/$(documents).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addclass("sticky");
+        }else{
+            $('.navbar').removeclass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addclass("show");
+        }
+        else{
+            $('.scroll-up-btn').removeclass("show");
+        }
+    });
+});
+
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+    
+    $('html').css("scrollBehavior","auto");
+});
+
+$('.navbar .menu li a').click(function(){
+    $('html').css("scrollBehavior","smooth");
+
+});
+
+$('.navbar .menu').toggleclass("active");
+$('.menu-btn i').toggleclass("active");
+
+
+
+var type=new Typed(".typing",{strings:["youTuber","Developer","Blogger","Designer","Freelancer"],
+   typeSpeed:100,
+   backSpeed:60,
+   loop:true
+});
+
+var typed=new Typed(".typing-2",{
+    string:["youTuber","Developer","Blogger","Designer","Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+$('.carousel').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplayTime:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false
+        }
+    }
+  });*/
+
+
+  (document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
